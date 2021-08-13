4fafffa6e62eb685f2b89b8cbf4132efa3f15f33
dominthebox <dsallustro@gmail.com>
2021-08-13 07:10:27 -0500

    set up index.js and server.js, server running, wip

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
 // build your server here and require it from index.js
+
+const express = require("express")
+
+const server = express()
+
+server.use(express.json())
+
+server.use('*', (req, res, next) => {
+    next({ status: 404, message:'not found' })
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 5000).json({
+        message: err.message
+    })
+})
+
+
+module.exports = server
